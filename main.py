6f6a49f09809dccee431488044a497000af57c93
Xendal Inc <>
2021-05-18 04:32:20 +0000

    deleted outdated files

--- main.py
+++ main.py
@@ -11,7 +11,6 @@ client = commands.Bot(command_prefix='$')
 @client.event
 async def on_ready():
 	client.load_extension('cogs.interactions')
-	#client.load_extension('cogs.queue_storage')
 	#client.load_extension('cogs.music')
 	print('We have logged in as {0.user}'.format(client))
 	await client.change_presence(status=discord.Status.do_not_disturb,activity=discord.Activity(type=discord.ActivityType.listening,name="XENDAL-Sama"))
